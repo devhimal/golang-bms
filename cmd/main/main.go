59ccbcf555c49612a9aaf9ac277c6b0ed24e6bc8
devhimal <himalhobbs.364@gmail.com>
2025-06-13 22:26:22 +0545

    feat: main.go file logic added in basic level

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,15 +4,16 @@ package main
 import (
 	"log"
 	"net/http"
-
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/devhimal/golang-bms/internal/routes"
 )
 
 func main() {
 	r := mux.NewRouter()
-	routes.RegisterRoutes(r)
-
+	routes.RegisterBooks(r)
+	http.Handle("/", r)
 	log.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
