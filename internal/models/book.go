419a5c201c78383f7f6492bf49c16aeb2f1e6efd
devhimal <himalhobbs.364@gmail.com>
2025-06-13 22:51:26 +0545

    feat: database actions are added to the book.go file

--- internal/models/book.go
+++ internal/models/book.go
@@ -22,3 +22,27 @@ func init(){
 	db.AutoMigrate(&Book{})
 }
 
+func ( b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookByID(Id int) (*Book, *gorm.DB) {
+	var getBook Book
+	result := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook,result
+}
+
+
+func deleteBook(Id int) Book {
+	var book Book
+	 db.Where("ID=?", Id).Delete(book)
+	return book
+}
